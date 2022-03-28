f99251e930fa17d740bcad3be83d4631aec8bb12
SeungWookHan <hswook12@me.com>
2022-03-28 13:01:55 +0900

    Add login component in home

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
 
+import KlipLoginButton from '../../components/KlipLoginButton';
+
 export default function Home() {
     return (
         <Box>
-            <Box sx={{ p: 2 }}>
-                이 페이지는 홈입니다.
-                <br />
-                로그인 한 유저의 피드와 종합적인 정보를 표시합니다.
-            </Box>
+            <Box sx={{ p: 2 }}>이 페이지는 홈입니다.</Box>
+            <KlipLoginButton url="http://naver.com" />
         </Box>
     );
 }
