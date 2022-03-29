a5c4d9f4c862fabd72eb84d5924d1bc45628bddb
SeungWookHan <hswook12@me.com>
2022-03-29 10:20:50 +0900

    Fix relative path to absolute path

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
 
-import KlipLoginButton from '../../components/KlipLoginButton';
+import LoginButton from '@components/LoginButton';
 
 export default function Home() {
     return (
         <Box>
             <Box sx={{ p: 2 }}>이 페이지는 홈입니다.</Box>
-            <KlipLoginButton url="http://naver.com" />
+            <LoginButton />
         </Box>
     );
 }
