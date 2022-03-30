b8e70459b97e6fad9a93e515bdd7a5e9b272f724
SeungWookHan <hswook12@me.com>
2022-03-30 12:46:53 +0900

    Add recoilvalue in Home.js

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Box } from '@mui/material';
-
+import { useRecoilValue } from 'recoil';
+import { userState } from '@recoil/user';
 import LoginButton from '@components/LoginButton';
 
 export default function Home() {
-    const [address, setAddress] = useState(''); // 추후 전역으로 상태 관리 예정
+    const user = useRecoilValue(userState);
 
     return (
         <Box>
             <Box sx={{ p: 2 }}>이 페이지는 홈입니다.</Box>
-            <LoginButton setAddress={setAddress} />
-            {address.length > 0 ? (
-                <Box sx={{ p: 2 }}>지갑 주소는 {address} 입니다.</Box>
+            <LoginButton />
+            {user.address.length > 0 ? (
+                <Box sx={{ p: 2 }}>지갑 주소는 {user.address} 입니다.</Box>
             ) : (
                 <Box sx={{ p: 2 }}>지갑 주소가 없습니다.</Box>
             )}
