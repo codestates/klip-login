97a0c079d15447efaa06c341f5d97232bf8e8b81
SeungWookHan <hswook12@me.com>
2022-04-05 23:41:10 +0900

    Add DIDApplicationButton in Home page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -5,6 +5,7 @@ import { userState } from '@recoil/user';
 import LoginButton from '@components/LoginButton';
 import LogoutButton from '@components/LogoutButton';
 import { isValidUser } from '@utils/user';
+import DIDApplicationButton from '@components/DIDApplicationButton';
 
 export default function Home() {
     const user = useRecoilValue(userState);
@@ -15,6 +16,7 @@ export default function Home() {
             {isValidUser(user) ? (
                 <Box sx={{ p: 2 }}>
                     <LogoutButton />
+                    <DIDApplicationButton />
                     지갑 주소는 {user.address} 입니다.
                 </Box>
             ) : (
