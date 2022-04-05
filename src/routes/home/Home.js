29e7e1921eea136447ca9bd48cbc7343db070c30
SeungWookHan <hswook12@me.com>
2022-04-05 15:14:12 +0900

    Add LogoutButton in Home page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import { useRecoilValue } from 'recoil';
 import { userState } from '@recoil/user';
 import LoginButton from '@components/LoginButton';
+import LogoutButton from '@components/LogoutButton';
 
 export default function Home() {
     const user = useRecoilValue(userState);
@@ -10,11 +11,16 @@ export default function Home() {
     return (
         <Box>
             <Box sx={{ p: 2 }}>이 페이지는 홈입니다.</Box>
-            <LoginButton />
             {user.address.length > 0 ? (
-                <Box sx={{ p: 2 }}>지갑 주소는 {user.address} 입니다.</Box>
+                <Box sx={{ p: 2 }}>
+                    <LogoutButton />
+                    지갑 주소는 {user.address} 입니다.
+                </Box>
             ) : (
-                <Box sx={{ p: 2 }}>지갑 주소가 없습니다.</Box>
+                <Box sx={{ p: 2 }}>
+                    <LoginButton />
+                    지갑 주소가 없습니다.
+                </Box>
             )}
         </Box>
     );
